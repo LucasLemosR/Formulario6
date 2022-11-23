6b02d0a8d7b359d45dfd7380f6c62879f9c7006a
LucasLemosR <105325775+LucasLemosR@users.noreply.github.com>
2022-11-23 18:29:08 -0300

    Add files via upload

---
+++ clientes.js
@@ -0,0 +1,15 @@
+const pessoas = [
+    { name: "Fabiana Araújo", age: 33 },
+    { name: "Gabriel Gomes", age: 25 },
+    { name: "Fernando Henrique", age: 17 },
+    { name: "Ana Luiza", age: 2 },
+    { name: "Geralda do Nascimento", age: 93 },
+    { name: "Miguel Souza", age: 70 },
+    { name: "Antonio Miguel", age: 69 },
+];
+
+function encontrarMaiores(pessoas) {
+    return pessoas.filter(function (pessoas) {
+     // idade >= 18 ?
+    })
+   }
